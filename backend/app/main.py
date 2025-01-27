e11c4f5a2ff34d827c80b566c2873f5a739eb79d
daniel.d <daniel.d@snowfalltravel.com>
2025-01-27 15:57:53 -0600

    feat: add cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.search_books import router as search_books_router
 
 app = FastAPI()
+
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
 app.include_router(search_books_router)
