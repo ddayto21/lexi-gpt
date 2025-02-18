c87556a915b63aa8be693d0016324374f8b222ec
daniel.d <daniel.d@snowfalltravel.com>
2025-02-18 14:57:03 -0600

    build(config): setup path aliases

---
+++ frontend/config-overrides.js
@@ -0,0 +1,11 @@
+const { override, addWebpackAlias } = require("customize-cra");
+const path = require("path");
+
+module.exports = override(
+  addWebpackAlias({
+    ["@components"]: path.resolve(__dirname, "src/components/"),
+    ["@utils"]: path.resolve(__dirname, "src/utils/"),
+    ["@services"]: path.resolve(__dirname, "src/services/"),
+    ["@tests"]: path.resolve(__dirname, "src/tests/"),
+  })
+);
