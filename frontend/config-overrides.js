98278274a2c5c3b40a93919248d34fa82e0ac922
daniel.d <daniel.d@snowfalltravel.com>
2025-02-18 18:56:53 -0600

    chore(config): add models to path alias

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -4,7 +4,9 @@ const path = require("path");
 module.exports = override(
   addWebpackAlias({
     ["@components"]: path.resolve(__dirname, "src/components/"),
+    ["@models"]: path.resolve(__dirname, "src/models/"),
     ["@utils"]: path.resolve(__dirname, "src/utils/"),
+    ["@lib"]: path.resolve(__dirname, "src/lib/"),
     ["@services"]: path.resolve(__dirname, "src/services/"),
     ["@tests"]: path.resolve(__dirname, "src/tests/"),
   })
